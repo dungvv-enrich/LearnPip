af87d911a5697be1a5d3c045933d6ef3069bea62
dungvv <vietdung1482000@gmail.com>
2024-03-18 21:18:38 +0700

    tro_ly_ao_basic

---
+++ hieu.py
@@ -0,0 +1,11 @@
+you = "today"
+
+if you == "":
+    robot = "I can't hear you, try again"
+elif you == "hello":
+    robot = "Hello, Dungvv"
+elif you == "today":
+    robot = "Today is satuday"
+else:
+    robot = "I'm fine thank you and you"
+print(robot)
---
+++ tro_ly_ao.py
@@ -0,0 +1,45 @@
+# import library
+import speech_recognition
+import pyttsx3
+from datetime import date, datetime
+
+# create variable
+robot_ear = speech_recognition.Recognizer()
+robot_mouth = pyttsx3.init()
+robot_brain = ""
+
+while True:
+    with speech_recognition.Microphone() as mic:
+        print("Robot: I'm Listening...")
+        audio = robot_ear.listen(mic)
+
+    print("Robot: ...")
+
+    try:
+        you = robot_ear.recognize_google(audio)
+    except:
+        you = ""
+
+    print("You: " + you)
+
+    if you == "":
+        robot_brain = "I can't hear you, try again"
+    elif "hello" in you:
+        robot_brain = "Hello, Dung"
+    elif "today" in you:
+        today = date.today()
+        robot_brain = today.strftime("%B %d, %Y")
+    elif "time" in you:
+        now = datetime.now()
+        robot_brain = now.strftime("%H hours %M minutes %S seconds")
+    elif "bye" in you:
+        robot_brain = "Bye, friend!"
+        print("robot_brain: " + robot_brain)
+        robot_mouth.say(robot_brain)
+        robot_mouth.runAndWait()
+        break
+    else:
+        robot_brain = "I'm fine thank you and you"
+    print("robot_brain: " + robot_brain)
+    robot_mouth.say(robot_brain)
+    robot_mouth.runAndWait()
